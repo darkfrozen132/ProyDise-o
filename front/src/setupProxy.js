42e1563739ee96016f2d27799d287aee3c465907
Leoncio Villanueva <68354609+darkfrozen132@users.noreply.github.com>
2025-11-03 18:27:38 -0500

    asd

--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
     '/api',
     createProxyMiddleware({
       //target: 'http://200.16.7.181:8000',
-      target: 'http://127.0.0.1',
+      target: 'http://127.0.0.1:8000',
       changeOrigin: true,
       secure: false,
       onProxyReq: (proxyReq, req, res) => {
